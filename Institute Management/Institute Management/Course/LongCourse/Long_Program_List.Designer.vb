670b9aaa397dff8cefa10c34a642e316e4abf9ad
sinaeean <31826916+sinaeean@users.noreply.github.com>
2017-12-12 22:46:11 +0700

    update

--- Institute Management/Institute Management/Course/LongCourse/Long_Program_List.Designer.vb
+++ Institute Management/Institute Management/Course/LongCourse/Long_Program_List.Designer.vb
@@ -24,6 +24,10 @@ Partial Class Long_Program_List
     Private Sub InitializeComponent()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.btnUpdate = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
         Me.LVCourse = New System.Windows.Forms.ListView()
         Me.ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
         Me.ColumnHeader2 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
@@ -39,10 +43,7 @@ Partial Class Long_Program_List
         Me.cboPrType = New System.Windows.Forms.ComboBox()
         Me.cboPrMainSubject = New System.Windows.Forms.ComboBox()
         Me.cbobach = New System.Windows.Forms.ComboBox()
-        Me.btnUpdate = New System.Windows.Forms.Button()
-        Me.btnDelete = New System.Windows.Forms.Button()
-        Me.btnAdd = New System.Windows.Forms.Button()
-        Me.btnClear = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
         Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
         '
@@ -72,6 +73,7 @@ Partial Class Long_Program_List
         Me.GroupBox1.Controls.Add(Me.Label12)
         Me.GroupBox1.Controls.Add(Me.Label6)
         Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.Label1)
         Me.GroupBox1.Controls.Add(Me.Label3)
         Me.GroupBox1.Controls.Add(Me.cboStuList)
         Me.GroupBox1.Controls.Add(Me.cboPrType)
@@ -84,115 +86,6 @@ Partial Class Long_Program_List
         Me.GroupBox1.Size = New System.Drawing.Size(910, 449)
         Me.GroupBox1.TabIndex = 20
         Me.GroupBox1.TabStop = False
-        '
-        'LVCourse
-        '
-        Me.LVCourse.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1, Me.ColumnHeader2, Me.ColumnHeader3, Me.ColumnHeader4, Me.ColumnHeader5})
-        Me.LVCourse.Location = New System.Drawing.Point(7, 81)
-        Me.LVCourse.Name = "LVCourse"
-        Me.LVCourse.Size = New System.Drawing.Size(790, 362)
-        Me.LVCourse.TabIndex = 32
-        Me.LVCourse.UseCompatibleStateImageBehavior = False
-        Me.LVCourse.View = System.Windows.Forms.View.Details
-        '
-        'ColumnHeader1
-        '
-        Me.ColumnHeader1.Text = "#ល.រ"
-        '
-        'ColumnHeader2
-        '
-        Me.ColumnHeader2.Text = "ជំនាន់/ក្រុម"
-        Me.ColumnHeader2.Width = 135
-        '
-        'ColumnHeader3
-        '
-        Me.ColumnHeader3.Text = "ឈ្មោះកម្មវិធី(ជំនាញ)"
-        Me.ColumnHeader3.Width = 235
-        '
-        'ColumnHeader4
-        '
-        Me.ColumnHeader4.Text = "ប្រភេទ(កម្រិត)កម្មវិធី"
-        Me.ColumnHeader4.Width = 155
-        '
-        'ColumnHeader5
-        '
-        Me.ColumnHeader5.Text = "បញ្ជីឈ្មោះសិស្ស"
-        Me.ColumnHeader5.Width = 205
-        '
-        'txtID
-        '
-        Me.txtID.Location = New System.Drawing.Point(5, 31)
-        Me.txtID.Name = "txtID"
-        Me.txtID.Size = New System.Drawing.Size(58, 32)
-        Me.txtID.TabIndex = 31
-        '
-        'Label12
-        '
-        Me.Label12.AutoSize = True
-        Me.Label12.Location = New System.Drawing.Point(665, 15)
-        Me.Label12.Name = "Label12"
-        Me.Label12.Size = New System.Drawing.Size(95, 24)
-        Me.Label12.TabIndex = 1
-        Me.Label12.Text = "បញ្ជីឈ្មោះសិស្ស"
-        '
-        'Label6
-        '
-        Me.Label6.AutoSize = True
-        Me.Label6.Location = New System.Drawing.Point(492, 15)
-        Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(123, 24)
-        Me.Label6.TabIndex = 1
-        Me.Label6.Text = "ប្រភេទ (កម្រិត) កម្មវិធី"
-        '
-        'Label5
-        '
-        Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(257, 15)
-        Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(152, 24)
-        Me.Label5.TabIndex = 1
-        Me.Label5.Text = "ឈ្មោះកម្មវិធីសិក្សា (ជំ​នាញ)"
-        '
-        'Label3
-        '
-        Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(107, 15)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(107, 24)
-        Me.Label3.TabIndex = 1
-        Me.Label3.Text = "ឈ្មោះជំនាន់​ / ក្រុម"
-        '
-        'cboStuList
-        '
-        Me.cboStuList.FormattingEnabled = True
-        Me.cboStuList.Location = New System.Drawing.Point(665, 43)
-        Me.cboStuList.Name = "cboStuList"
-        Me.cboStuList.Size = New System.Drawing.Size(132, 32)
-        Me.cboStuList.TabIndex = 0
-        '
-        'cboPrType
-        '
-        Me.cboPrType.FormattingEnabled = True
-        Me.cboPrType.Location = New System.Drawing.Point(492, 43)
-        Me.cboPrType.Name = "cboPrType"
-        Me.cboPrType.Size = New System.Drawing.Size(168, 32)
-        Me.cboPrType.TabIndex = 0
-        '
-        'cboPrMainSubject
-        '
-        Me.cboPrMainSubject.FormattingEnabled = True
-        Me.cboPrMainSubject.Location = New System.Drawing.Point(257, 43)
-        Me.cboPrMainSubject.Name = "cboPrMainSubject"
-        Me.cboPrMainSubject.Size = New System.Drawing.Size(231, 32)
-        Me.cboPrMainSubject.TabIndex = 0
-        '
-        'cbobach
-        '
-        Me.cbobach.FormattingEnabled = True
-        Me.cbobach.Location = New System.Drawing.Point(107, 43)
-        Me.cbobach.Name = "cbobach"
-        Me.cbobach.Size = New System.Drawing.Size(144, 32)
-        Me.cbobach.TabIndex = 0
         '
         'btnUpdate
         '
@@ -238,6 +131,124 @@ Partial Class Long_Program_List
         Me.btnClear.Text = "Clear"
         Me.btnClear.UseVisualStyleBackColor = False
         '
+        'LVCourse
+        '
+        Me.LVCourse.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1, Me.ColumnHeader2, Me.ColumnHeader3, Me.ColumnHeader4, Me.ColumnHeader5})
+        Me.LVCourse.Location = New System.Drawing.Point(7, 81)
+        Me.LVCourse.Name = "LVCourse"
+        Me.LVCourse.Size = New System.Drawing.Size(790, 362)
+        Me.LVCourse.TabIndex = 32
+        Me.LVCourse.UseCompatibleStateImageBehavior = False
+        Me.LVCourse.View = System.Windows.Forms.View.Details
+        '
+        'ColumnHeader1
+        '
+        Me.ColumnHeader1.Text = "#ល.រ"
+        '
+        'ColumnHeader2
+        '
+        Me.ColumnHeader2.Text = "ជំនាន់/ក្រុម"
+        Me.ColumnHeader2.Width = 135
+        '
+        'ColumnHeader3
+        '
+        Me.ColumnHeader3.Text = "ឈ្មោះកម្មវិធី(ជំនាញ)"
+        Me.ColumnHeader3.Width = 235
+        '
+        'ColumnHeader4
+        '
+        Me.ColumnHeader4.Text = "ប្រភេទ(កម្រិត)កម្មវិធី"
+        Me.ColumnHeader4.Width = 155
+        '
+        'ColumnHeader5
+        '
+        Me.ColumnHeader5.Text = "បញ្ជីឈ្មោះសិស្ស"
+        Me.ColumnHeader5.Width = 205
+        '
+        'txtID
+        '
+        Me.txtID.Location = New System.Drawing.Point(7, 39)
+        Me.txtID.Name = "txtID"
+        Me.txtID.Size = New System.Drawing.Size(81, 32)
+        Me.txtID.TabIndex = 31
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(665, 15)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(95, 24)
+        Me.Label12.TabIndex = 1
+        Me.Label12.Text = "បញ្ជីឈ្មោះសិស្ស"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(492, 15)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(123, 24)
+        Me.Label6.TabIndex = 1
+        Me.Label6.Text = "ប្រភេទ (កម្រិត) កម្មវិធី"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(243, 15)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(152, 24)
+        Me.Label5.TabIndex = 1
+        Me.Label5.Text = "ឈ្មោះកម្មវិធីសិក្សា (ជំ​នាញ)"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(125, 18)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(107, 24)
+        Me.Label3.TabIndex = 1
+        Me.Label3.Text = "ឈ្មោះជំនាន់​ / ក្រុម"
+        '
+        'cboStuList
+        '
+        Me.cboStuList.FormattingEnabled = True
+        Me.cboStuList.Location = New System.Drawing.Point(665, 43)
+        Me.cboStuList.Name = "cboStuList"
+        Me.cboStuList.Size = New System.Drawing.Size(132, 32)
+        Me.cboStuList.TabIndex = 0
+        '
+        'cboPrType
+        '
+        Me.cboPrType.FormattingEnabled = True
+        Me.cboPrType.Location = New System.Drawing.Point(480, 43)
+        Me.cboPrType.Name = "cboPrType"
+        Me.cboPrType.Size = New System.Drawing.Size(180, 32)
+        Me.cboPrType.TabIndex = 0
+        '
+        'cboPrMainSubject
+        '
+        Me.cboPrMainSubject.FormattingEnabled = True
+        Me.cboPrMainSubject.Location = New System.Drawing.Point(243, 43)
+        Me.cboPrMainSubject.Name = "cboPrMainSubject"
+        Me.cboPrMainSubject.Size = New System.Drawing.Size(231, 32)
+        Me.cboPrMainSubject.TabIndex = 0
+        '
+        'cbobach
+        '
+        Me.cbobach.FormattingEnabled = True
+        Me.cbobach.Location = New System.Drawing.Point(94, 42)
+        Me.cbobach.Name = "cbobach"
+        Me.cbobach.Size = New System.Drawing.Size(144, 32)
+        Me.cbobach.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(7, 12)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(58, 24)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "លេខរៀង"
+        '
         'Long_Program_List
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
@@ -273,4 +284,5 @@ Partial Class Long_Program_List
     Friend WithEvents btnDelete As System.Windows.Forms.Button
     Friend WithEvents btnAdd As System.Windows.Forms.Button
     Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
 End Class
